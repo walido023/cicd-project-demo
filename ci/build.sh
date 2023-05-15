28ae55704b1c071dad6d8f9fe24729de6b6ee7dd
walid khelil <walid_khelil@carrefour.com>
2023-05-15 21:46:00 +0200

    add ci/build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,5 @@
 # Build and publish docker image
 GIT_TAG=:${GITHUB_REF#refs/tags/}
 DOCKER_TAG=${GIT_TAG#:refs/heads/main}
-./mvnw -ntp jib:build -Djib.to.image=${{ fromJson(steps.config.outputs.config).image_name }}${DOCKER_TAG}  -Pprod
+#./mvnw -ntp jib:build -Djib.to.image=${{ fromJson(steps.config.outputs.config).image_name }}${DOCKER_TAG}  -Pprod
+./mvnw -ntp jib:build -Djib.to.image=europe-west9-docker.pkg.dev/s4a-cicd-poc-sbx-5b20/demo-repo/cicd-demo:latest  -Pprod
